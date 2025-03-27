5e419e8234887c3f060cd2da4e13403d97fd9c8e
luckyuyuy <lucasmarigo12gamil.com>
2025-03-27 11:07:19 -0300

    criacao da tela de cadastro

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/criar-conta', function () {
+    return view('criar-conta');
 });
 Route::get('/soma/{num1}/{num2}', function ($num1, $num2) {
     return 'soma atual é: '.$num1 + $num2; 
