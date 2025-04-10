8bf6c1189795c294aedb12c24f8dcf70670d624c
luckyuyuy <lucasmarigo12@gmail.com>
2025-04-10 10:45:28 -0300

    tela de cadastro salvando

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\User;
+use iLLuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,7 +10,18 @@ Route::get('/', function () {
 
 Route::get('/criar-conta', function () {
     return view('criar-conta');
-});
+}); 
+Route::post('/salvar-conta', function (request $request) {
+    //dd($request);
+    $user = new User();
+    $user->name = $request->name;
+    $user->email = $request->email;
+    $user->password = $request->password;
+    $user->save();
+
+    return "usuarip salvo com sucesso";
+})->name('salvar-conta');
+
 Route::get('/soma/{num1}/{num2}', function ($num1, $num2) {
     return 'soma atual é: '.$num1 + $num2; 
 });
